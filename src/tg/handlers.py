4f32f7674f25df1544937cc0af023a68893e25ac
paulin-mipt <paulin.mipt@gmail.com>
2020-04-16 21:27:18 +0100

    First stats version

--- src/tg/handlers.py
+++ src/tg/handlers.py
@@ -12,17 +12,21 @@ logger = logging.getLogger(__name__)
 # Command handlers
 def start(update, context):
     # TODO: register a new user somewhere, e.g. Google Sheet
-    update.message.reply_text('Welcome text will be here!')
+    update.message.reply_text('Ура! Теперь ты можешь получать обновления от этого бота!')
 
 
 def help(update, context):
     # TODO: add some help text
-    update.message.reply_text('Help text will be here')
+    update.message.reply_text('Здесь будет какая-нибудь инструкция')
 
 
 def test_handler(update, context):
     """Handler for /test command, feel free to use it for one-off job testing"""
-    jobs.manager_stats_job(bot.trello_client, bot.telegram_sender)
+    jobs.manager_stats_job(
+        bot.trello_client,
+        bot.telegram_sender,
+        bot.config['trello']['_tmp_']['list_aliases']  # TODO: remove this
+    )
 
 
 # Other handlers
