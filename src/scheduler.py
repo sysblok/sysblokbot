7f3688cef1a75ec0b786339b59f61cb3af97708d
Kulikov Alexey <alexeyqu@gmail.com>
2020-04-17 17:01:00 +0100

    config scheduler (#9)

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,7 +1,11 @@
+import logging
 import schedule
 import telegram
 
 from .jobs import jobs
+
+
+logger = logging.getLogger(__name__)
 
 
 class JobScheduler:
@@ -12,11 +16,21 @@ class JobScheduler:
         self.sheets_client = sheets_client
 
     def init_jobs(self):
-        # TODO: parse config for scheduled time
-        schedule.every().wednesday.at("13:15").do(
-            jobs.sample_job,
-            trello_client=self.trello_client,
-            sheets_client=self.sheets_client,
-            telegram_sender=self.telegram_sender
-        )
-
+        for job_id, schedule_dict in self.config['jobs'].items():
+            try:
+                job = getattr(jobs, job_id)
+            except Exception as e:
+                logger.error(f'Job {job_id} not found: {e}')
+                continue
+            try:
+                scheduled = getattr(schedule.every(), schedule_dict['every'])
+                if 'at' in schedule_dict:
+                    scheduled = scheduled.at(schedule_dict['at'])
+                scheduled.do(
+                    job,
+                    trello_client=self.trello_client,
+                    sheets_client=self.sheets_client,
+                    telegram_sender=self.telegram_sender
+                )
+            except Exception as e:
+                logger.error(f'Failed to schedule job {job_id} with params {schedule_dict}: {e}')
