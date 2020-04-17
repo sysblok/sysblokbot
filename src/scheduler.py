b18c1be75804805df745af28f40b7ee4af2a4e19
Alexey Kulikov <alexeyqu@gmail.com>
2020-04-17 02:08:18 +0100

    config scheduler

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,7 +1,11 @@
+import logging
 import schedule
 import telegram
 
 from .jobs import jobs
+
+
+logger = logging.getLogger(__name__)
 
 
 class JobScheduler:
@@ -12,11 +16,26 @@ class JobScheduler:
         self.sheets_client = sheets_client
 
     def init_jobs(self):
-        # TODO: parse config for scheduled time
-        schedule.every().wednesday.at("13:15").do(
-            jobs.sample_job,
-            trello_client=self.trello_client,
-            sheets_client=self.sheets_client,
-            telegram_sender=self.telegram_sender
-        )
-
+        for job_id, schedule_dict in self.config['jobs'].items():
+            try:
+                job = getattr(jobs, job_id)
+            except Exception as e:
+                logger.error(f'Job {job_id} not found: {e}')
+                continue
+            try:
+                if 'at' in schedule_dict:
+                    getattr(schedule.every(), schedule_dict['every']).at(schedule_dict['at']).do(
+                        job,
+                        trello_client=self.trello_client,
+                        sheets_client=self.sheets_client,
+                        telegram_sender=self.telegram_sender
+                    )
+                else:
+                    getattr(schedule.every(), schedule_dict['every']).do(
+                        job,
+                        trello_client=self.trello_client,
+                        sheets_client=self.sheets_client,
+                        telegram_sender=self.telegram_sender
+                    )
+            except Exception as e:
+                logger.error(f'Failed to schedule job {job_id} with params {schedule_dict}: {e}')
---
+++ tests/test_scheduler.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src import scheduler
+from src.jobs import jobs
+
+
+@pytest.mark.parametrize(
+    "jobs_config, num_jobs",
+    [
+        ({"job": {"every": "day"}}, 1),
+        ({"job": {"every": "day", "at": "10:00"}}, 1),
+        ({"job": {"every": "monday", "at": "10:00"}}, 1),
+        ({"job": {"every": "tuesday", "at": "10:00"}}, 1),
+        ({"job": {"every": "wednesday", "at": "10:00"}}, 1),
+        ({"job": {"every": "thursday", "at": "10:00"}}, 1),
+        ({"job": {"every": "friday", "at": "10:00"}}, 1),
+        ({"job": {"every": "saturday", "at": "10:00"}}, 1),
+        ({"job": {"every": "sunday", "at": "10:00"}}, 1),
+        ({"job": {"every": "hour"}}, 1),
+        ({"job": {"every": "day", "at": "10"}}, 0),
+        ({"job": {"every": "hour", "at": "10:00"}}, 0),
+    ]
+)
+def test_scheduler(monkeypatch, jobs_config, num_jobs):
+    for job_id in jobs_config:
+        setattr(jobs, job_id, lambda _: 0)
+
+    scheduler.JobScheduler({'jobs': jobs_config}, None, None, None)
