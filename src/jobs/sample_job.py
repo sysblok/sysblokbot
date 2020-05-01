6fd5e4db9ce287140c3e196f1fd505d42fbdd27c
Kulikov Alexey <alexeyqu@gmail.com>
2020-05-01 23:56:53 +0100

    logging (#54)

--- src/jobs/sample_job.py
+++ src/jobs/sample_job.py
@@ -1,8 +1,10 @@
 from typing import Callable
 
 from ..app_context import AppContext
+from .utils import job_log_start_stop
 
 
+@job_log_start_stop
 def execute(app_context: AppContext, send: Callable[[str], None]):
     # Logic here could include retrieving data from trello/sheets
     # and sending a notification to corresponding user.
