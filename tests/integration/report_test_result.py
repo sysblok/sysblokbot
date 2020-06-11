f6e97ab84be188f26121f6bc8b8cafadaee4485e
Kulikov Alexey <alexeyqu@gmail.com>
2020-06-11 02:57:57 +0100

    better error report (#112)

--- tests/integration/report_test_result.py
+++ tests/integration/report_test_result.py
@@ -12,9 +12,10 @@ api_id = int(os.environ["TELEGRAM_APP_ID"])
 api_hash = os.environ["TELEGRAM_APP_HASH"]
 session_str = os.environ["TELETHON_SESSION"]
 telegram_chat_id = int(os.environ["TELEGRAM_ERROR_CHAT_ID"])
+telegram_bot_name = os.environ["TELEGRAM_BOT_NAME"]
 
 
-async def report_test_result(passed: bool):
+async def report_test_result(passed: bool, failed_tests: str=''):
     client = TelegramClient(
         StringSession(session_str), api_id, api_hash,
         sequential_updates=True
@@ -25,7 +26,11 @@ async def report_test_result(passed: bool):
     await client.get_me()
 
     async with client.conversation(telegram_chat_id, timeout=30) as conv:
-        message = 'Протестировано, ок на выкладку.' if passed else 'Тестинг разломан, не катимся.'
+        if passed:
+            message = 'Протестировано, ок на выкладку.'
+        else:
+            failed_cmds = '\n'.join(f'{cmd.strip()}{telegram_bot_name}' for cmd in failed_tests)
+            message = f'Тестинг разломан, не катимся.\nСломались команды:\n{failed_cmds}'
         await conv.send_message(message)
 
     await client.disconnect()
@@ -33,4 +38,9 @@ async def report_test_result(passed: bool):
 
 
 if __name__ == '__main__':
-    asyncio.run(report_test_result(sys.argv[1] == '0'))
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as failed_tests_file:
+            failed_tests = failed_tests_file.readlines()
+    else:
+        failed_tests = []
+    asyncio.run(report_test_result(len(failed_tests) == 0, failed_tests=failed_tests))
