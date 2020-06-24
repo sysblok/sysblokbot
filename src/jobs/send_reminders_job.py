d41bcc3c3b74ca90d69089adc661d4a14e4b955b
Polina Matavina <paulin.mipt@gmail.com>
2020-06-25 00:45:13 +0100

    add file

---
+++ src/jobs/send_reminders_job.py
@@ -0,0 +1,18 @@
+import logging
+from typing import Callable
+
+from ..app_context import AppContext
+from ..tg.sender import TelegramSender
+from .base_job import BaseJob
+
+logger = logging.getLogger(__name__)
+
+
+class SendRemindersJob(BaseJob):
+    @staticmethod
+    def _execute(app_context: AppContext, send: Callable[[str], None]):
+        sender = TelegramSender()
+        reminders = app_context.db_client.get_reminders_to_send()
+        for reminder in reminders:
+            sender.send_to_chat_id(reminder.text, reminder.group_chat_id)
+        send(f'Sent {len(reminders)} reminders')
