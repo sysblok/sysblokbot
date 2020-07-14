6d44161de6e8ffa3c360c0cc93bf9b1684e90f1c
Polina Matavina <paulin.mipt@gmail.com>
2020-07-14 19:06:46 +0100

    merge

--- src/jobs/send_reminders_job.py
+++ src/jobs/send_reminders_job.py
@@ -19,8 +19,4 @@ class SendRemindersJob(BaseJob):
                 sender.send_to_chat_id(reminder.text, reminder.group_chat_id)
             else:
                 logger.info(f'Reminder {reminder.name} not sent (deactivated)')
-<<<<<<< HEAD
-        send(f'Sent {len(reminders)} reminders')
-=======
         send(load('send_reminders_job__success', length=len(reminders)))
->>>>>>> af66cc68352ad5e1c2f34276af7d6161546264f0
