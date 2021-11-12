285fa3b141adbc51f97b59baa1c685e4d3de96f0
Alexey Kulikov <alexeyqu@gmail.com>
2021-11-12 02:15:03 +0000

    fix

--- src/jobs/shrug_job.py
+++ src/jobs/shrug_job.py
@@ -8,4 +8,4 @@ from .base_job import BaseJob
 class ShrugJob(BaseJob):
     @staticmethod
     def _execute(app_context: AppContext, send: Callable[[str], None], called_from_handler=False):
-        send('¯\_(ツ)_/¯')
+        send(load('shrug'))
