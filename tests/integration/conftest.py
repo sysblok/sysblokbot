528be93ad6c81d4e8e1017459a4833e90cc683f0
Alexey Kulikov <alexeyqu@gmail.com>
2021-11-28 23:05:54 +0000

    test tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,12 @@
+import asyncio
 import json
 import pytest
 import os
+from typing import List
+
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+
 
 if os.path.exists('config_override.json'):
     with open('config_override.json') as config_override:
@@ -18,7 +22,7 @@ telegram_bot_name = config.get("handle", '')
 
 
 @pytest.fixture
-async def client() -> TelegramClient:
+async def telegram_client() -> TelegramClient:
     client = TelegramClient(
         StringSession(api_session), api_id, api_hash,
         sequential_updates=True
@@ -30,5 +34,56 @@ async def client() -> TelegramClient:
 
     yield client
 
+    await client.disconnect()
+    await client.disconnected
+
+
+def pytest_sessionstart(session):
+    session.results = dict()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == 'call':
+        item.session.results[item] = result
+
+
+def pytest_sessionfinish(session, exitstatus):
+    passed = exitstatus == pytest.ExitCode.OK
+    print('run status code:', exitstatus)
+    passed_tests = [
+        result.longreprtext for result in session.results.values() if result.passed
+    ]
+    failed_tests = [
+        result.longreprtext for result in session.results.values() if result.failed
+    ]
+    print(f'{len(passed_tests)} passed\n{", ".join(failed_tests)}\n')
+    print(f'{len(failed_tests)} failed\n{", ".join(failed_tests)}\n')
+    asyncio.run(report_test_result(passed, failed_tests))
+
+
+async def report_test_result(passed: bool, failed_tests: List[str] = []):
+    client = TelegramClient(
+        StringSession(api_session), api_id, api_hash,
+        sequential_updates=True
+    )
+    # Connect to the server
+    await client.connect()
+    # Issue a high level command to start receiving message
+    await client.get_me()
+    async with client.conversation(telegram_chat_id, timeout=30) as conv:
+        if passed:
+            message = f'@{telegram_bot_name} протестирован.'
+        else:
+            failed_cmds = '\n'.join(
+                f'{cmd.strip()}@{telegram_bot_name}'
+                for cmd in failed_tests
+            )
+            message = f'@{telegram_bot_name} разломан.\nСломались команды:\n{failed_cmds}'
+        await conv.send_message(message)
+    # disconnect
     await client.disconnect()
     await client.disconnected
--- tests/integration/test_telegram_bot.py
+++ tests/integration/test_telegram_bot.py
@@ -14,15 +14,30 @@ from conftest import telegram_bot_name
     'command',
     (
         '/start',
+        '/get_ig_analytics_report',
         '/get_trello_board_state',
         '/get_publication_plans',
         '/get_editorial_report',
         '/bad_cmd',
     )
 )
-async def test_not_failing(client: TelegramClient, command: str):
-    # Create a conversation
-    async with client.conversation(telegram_bot_name, timeout=120) as conv:
+async def test_not_failing(telegram_client: TelegramClient, command: str):
+    async with telegram_client.conversation(telegram_bot_name, timeout=120) as conv:
         await conv.send_message(command)
         resp: Message = await conv.get_response()
         assert resp.raw_text
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    'command',
+    (
+        '/bad_cmd',
+    )
+)
+async def test_failing(telegram_client: TelegramClient, command: str):
+    async with telegram_client.conversation(telegram_bot_name, timeout=10) as conv:
+        await conv.send_message(command)
+        resp: Message = await conv.get_response()
+        assert resp.raw_text is None
