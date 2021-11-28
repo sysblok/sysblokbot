b49607a4f1d458f4275ad2dc5c00ea36151ac4b2
Paulin Matavina <paulin.mipt@gmail.com>
2021-11-28 00:38:40 +0000

    add parametrized get_report_from_sheet handler

--- src/jobs/sheet_report_job.py
+++ src/jobs/sheet_report_job.py
@@ -4,6 +4,7 @@ from typing import Callable, List
 import re
 
 from ..app_context import AppContext
+from ..consts import KWARGS
 from ..strings import load
 from .base_job import BaseJob
 
@@ -25,12 +26,21 @@ class SheetReportJob(BaseJob):
             app_context: AppContext,
             send: Callable[[str], None],
             called_from_handler=False,
+            *args,
             **kwargs
     ):
         logger.info(f'Getting sheet report for: {kwargs.get("name")}')
         if called_from_handler:
-            pretty_send(['TODO: debug mode not working yet'], send)
-            return
+            schedules = app_context.config_manager.get_jobs_config(__name__.split('.')[-1])
+            if len(args) == 0:
+                names = [schedule.get(KWARGS, {}).get("name") for schedule in schedules]
+                send(f'Usage: /get_report_from_sheet name\nAvailable names: {names}')
+                return
+            assert len(args) == 1
+            name = args[0]
+            for schedule in schedules:
+                if schedule.get(KWARGS, {}).get('name') == name:
+                    kwargs = schedule[KWARGS]
         spreadsheet_key = kwargs['spreadsheet_key']
         sheet_name = kwargs.get('sheet_name')
         message_template = load(
