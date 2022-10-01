5bc174f89f91de8be7fd3c4b0c0d61e99cb04f45
Alexey Kulikov <alexeyqu@gmail.com>
2022-10-01 11:28:12 +0100

    add url to sheet report job

--- src/jobs/sheet_report_job.py
+++ src/jobs/sheet_report_job.py
@@ -46,7 +46,8 @@ class SheetReportJob(BaseJob):
         sheet_name = kwargs.get('sheet_name')
         message_template = load(
             kwargs['template_string'],
-            date=datetime.now().strftime('%d.%m.%Y')
+            date=datetime.now().strftime('%d.%m.%Y'),
+            url=f'https://docs.google.com/spreadsheets/d/{spreadsheet_key}'
         )
         sheet = app_context.sheets_client.fetch_sheet(spreadsheet_key, sheet_name)
         message_template_substituted = message_template
