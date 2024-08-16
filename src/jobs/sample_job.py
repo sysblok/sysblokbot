803cf7cebf5197db72b3a4d9888deddedb0ee5db
Konstantin Gurashkin <crocko@mail.ru>
2024-08-16 13:25:52 +0300

    feat: SampleJob

--- src/jobs/sample_job.py
+++ src/jobs/sample_job.py
@@ -7,10 +7,10 @@ from .base_job import BaseJob
 
 class SampleJob(BaseJob):
     @staticmethod
-    def _execute(
+    async def _execute(
         app_context: AppContext, send: Callable[[str], None], called_from_handler=False
     ):
         # Logic here could include retrieving data from trello/sheets
         # and sending a notification to corresponding user.
         # app_context contain all necessary clients inside.
-        send(load("sample_job__string", status="done"))
+        await send(load("sample_job__string", status="done"))
