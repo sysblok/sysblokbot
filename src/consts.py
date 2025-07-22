1ae261817dfc5d09d01c6a38f692df62268382b4
tabarzin <tabarzin@gmail.com>
2025-07-22 22:26:09 +0300

    feat: add new alias

--- src/consts.py
+++ src/consts.py
@@ -116,6 +116,22 @@ class TrelloListAlias(Enum):
     BACK_BURNER = "trello_list_name__back_burner"
 
 
+class BoardListAlias(Enum):
+    TOPIC_SUGGESTION_1 = "board_list_name__topic_suggestion_1"
+    TOPIC_READY_2 = "board_list_name__topic_ready_2"
+    DRAFT_N_PROGRESS_3 = "board_list_name__draft_n_progress_3"
+    DRAFT_COMPLETED_4 = "board_list_name__draft_completed_4"
+    PENDING_SEO_EDITOR_6 = "board_list_name__pending_seo_editor_6"
+    PENDING_EDITOR_5 = "board_list_name__pending_editor_5"
+    APPROVED_EDITOR_7 = "board_list_name__approved_editor_7"
+    PENDING_CHIEF_EDITOR_8 = "board_list_name__pending_chief_editor_8"
+    PUBLISH_BACKLOG_9 = "board_list_name__publish_backlog_9"
+    PUBLISH_IN_PROGRESS_10 = "board_list_name__publish_in_progress_10"
+    PUBLISH_DONE_11 = "board_list_name__publish_done_11"
+    BACK_BURNER = "trello_list_name__back_burner"
+    BACK_BURNER_PUBLISHED = "board_list_name__back_burner_published"
+
+
 class TrelloCustomFieldTypeAlias(Enum):
     AUTHOR = "trello_custom_field__author"
     GOOGLE_DOC = "trello_custom_field__google_doc"
